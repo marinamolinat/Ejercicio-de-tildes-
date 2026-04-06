91510d2d06d56df23ada836837612eab4439cb9f
Marina M <marinamolina7877@gmail.com>
2026-04-06 08:26:58 -0500

    counter working + list of words that the user has gotten wrong

--- script.js
+++ script.js
@@ -3,32 +3,63 @@ var r;
 mostrarPalabra();
 
 
+var correcto = 0;
+var incorrecto = 0;
+
 async function fetchPalabra() {
-const response = await fetch('./palabras.txt');
-const text = await response.text();
-const lines = text.split('\n');
-const palabra = lines[Math.floor(Math.random() * lines.length)].trim();
 
-
-// resvisar si tiene tilde 
-let index = -1;
-for (let i = 0; i < palabra.length; i++) {
-
- 
-    if (palabra[i] === 'á' || palabra[i] === 'é' || palabra[i] === 'í' || palabra[i] === 'ó' || palabra[i] === 'ú') {
-        index = i;
-        break;
+    const moneda = Math.round(Math.random());
+    let text = '';
+    if (moneda === 0) {
+        const response = await fetch('./noTildes.txt');
+        text = await response.text();
+    }
+    else {
+        const response = await fetch('./tildes.txt');
+        text = await response.text();
 
     }
+    
+    const lines = text.split('\n');
+    const palabra = lines[Math.floor(Math.random() * lines.length)].trim();
 
-}
-resultado = {
-    palabra: palabra,
-    index: index
-}
-r = resultado
-return resultado
-}
+
+    // resvisar si tiene tilde 
+    let index = -1;
+    const accentMap = {
+        "á": "a",
+        "é": "e",
+        "í": "i", 
+        "ó": "o",
+        "ú": "u"
+    };
+
+    let newPalabra = ''; 
+
+    for (let i = 0; i < palabra.length; i++) {
+    
+    
+        if (palabra[i] === 'á' || palabra[i] === 'é' || palabra[i] === 'í' || palabra[i] === 'ó' || palabra[i] === 'ú') {
+            index = i;
+            newPalabra += accentMap[palabra[i]];
+      
+
+
+
+        }
+        else {
+            newPalabra += palabra[i];
+        }
+        
+
+    }
+    let resultado = {
+        palabra: newPalabra,
+        index: index
+    }
+    r = resultado
+    return resultado
+    }
 
 
 function mostrarPalabra() {
@@ -97,7 +128,12 @@ var boton = true
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function bien(){
-     boton = false
+
+
+    correcto++;
+    document.getElementById("correcto").innerText = `Correcto ${correcto}`;
+
+    boton = false
 
     mostrarPalabra(); 
     console.log('bien');
@@ -107,6 +143,7 @@ async function bien(){
     setTimeout(() => body.classList.remove('flashBien'), 500);
     await sleep(500);
     boton = true
+
     
     
 
@@ -114,18 +151,47 @@ async function bien(){
 }
 
 async function mal(){
+    incorrecto++;
+
+    document.getElementById("incorrecto").innerText = `Incorrecto: ${incorrecto}`;
+
     boton = false
     mostrarPalabra(); 
 
     
     const body = document.querySelector('body')
     body.classList.add('flashMal');
-    setTimeout(() => body.classList.remove('flashMal'), 500);
-    
+    setTimeout(() => body.classList.remove('flashMal'),  500);
+
+    //convertir palabra back to tilde 
+
+    const accentMapRerverse = {
+        "a": "á",
+        "e": "é",
+        "i": "í",
+        "o": "ó",
+        "u": "ú"
+    };
+    newPalabra = '';
+    for (let i = 0; i < r.palabra.length; i++) {
+        if (i === r.index) {
+            newPalabra += accentMapRerverse[r.palabra[i]];
+        }
+        else {
+            newPalabra += r.palabra[i];
+        }
+    }
+
+
+
+    document.getElementById('palabrasMalas').innerHTML += `<li>${newPalabra}</li>`;
     
     console.log('mal');
-     await sleep(500);
+    await sleep(500);
     boton = true
+
+
+
 
     
 }
